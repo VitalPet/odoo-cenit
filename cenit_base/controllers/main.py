06f61161acb70280e6bc6304c8e795813107c755
esousy <esousy@gmail.com>
2017-07-30 20:01:00 +0300

    [MIG] Migration to Odoo 10.0

--- cenit_base/controllers/main.py
+++ cenit_base/controllers/main.py
@@ -3,10 +3,10 @@
 import logging
 import inflect
 
-from openerp import http
-from openerp import SUPERUSER_ID
-from openerp.http import request
-from openerp.modules.registry import RegistryManager
+from odoo import http
+from odoo import SUPERUSER_ID
+from odoo.http import request
+from odoo.modules.registry import Registry
 
 
 _logger = logging.getLogger(__name__)
@@ -30,31 +30,30 @@ class WebhookController(http.Controller):
             db_name = host.replace(".", "_").split(":")[0]
 
         # if db_name in http.db_list():
-        registry = RegistryManager.get(db_name)
+        registry = Registry(db_name)
         with registry.cursor() as cr:
-            connection_model = registry['cenit.connection']
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            connection_model = env['cenit.connection']
             domain = [('key', '=', key), ('token', '=', token)]
             _logger.info(
                 "Searching for a 'cenit.connection' with key '%s' and "
                 "matching token", key)
-            rc = connection_model.search(cr, SUPERUSER_ID, domain)
+            rc = connection_model.sudo().search(domain)
             _logger.info("Candidate connections: %s", rc)
             if rc:
                 p = inflect.engine()
-                flow_model = registry['cenit.flow']
+                flow_model = env['cenit.flow']
                 context = {'sender': 'client', 'action': action}
 
                 if root is None:
                     for root, data in request.jsonrequest.items():
                         root = p.singular_noun(root) or root
-                        rc = flow_model.receive(cr, SUPERUSER_ID, root,
-                                                data, context)
+                        rc = flow_model.receive(root, data)
                         if rc:
                             status_code = 200
                 else:
                     root = p.singular_noun(root) or root
-                    rc = flow_model.receive(cr, SUPERUSER_ID, root,
-                                            request.jsonrequest, context)
+                    rc = flow_model.receive( root, request.jsonrequest)
                     if rc:
                         status_code = 200
             else:
