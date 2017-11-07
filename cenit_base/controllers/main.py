a11d3205e31f2efa6a671674f86ebfa886d9e5a2
esousy <esousy@gmail.com>
2017-11-07 17:07:21 +0200

    [FIX] Convert json

--- cenit_base/controllers/main.py
+++ cenit_base/controllers/main.py
@@ -71,8 +71,10 @@ class WebhookController(http.Controller):
                         _logger.error("new root: %s", root)
                         if isinstance(data, basestring):
                             data = literal_eval(data)
+                        else:
+                            data = json.loads(data)
                         for record in data:
-                            #record = json.loads(r)
+                            # record = json.loads(record)
                             _logger.error("new record: %s", record)
                             rc = flow_model.receive(root, record)
                             if rc:
