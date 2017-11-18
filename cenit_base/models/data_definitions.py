ea3f0d2790bad13d8c40f1e64d9e1973f2b934ad
esousy <esousy@gmail.com>
2017-11-18 05:18:19 +0200

    [FIX] Inactive Records

--- cenit_base/models/data_definitions.py
+++ cenit_base/models/data_definitions.py
@@ -538,7 +538,6 @@ class CenitDataType(models.Model):
             elif isinstance(domain[0], list):
                 domain = domain[0]
             domain.append(("id", "=", obj.id))
-            domain.append( ('active','=', False))
             _logger.info("\n\nensure_object: %s\n", domain)
             match = self.env[obj._name].search(domain) or False
         _logger.info("\n\nensure_object match: %s\n", match)
